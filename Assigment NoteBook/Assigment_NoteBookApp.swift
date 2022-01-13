cd1085b520a1a8747229c7d1df55bd7c8cadf4a4
Kemushi <tigeramano@gmail.com>
2022-01-13 10:42:38 -0600

    Initial Commit

---
+++ Assigment NoteBook/Assigment_NoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assigment_NoteBookApp.swift
+//  Assigment NoteBook
+//
+//  Created by Student on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Assigment_NoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assigment NoteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assigment NoteBook
+//
+//  Created by Student on 1/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
